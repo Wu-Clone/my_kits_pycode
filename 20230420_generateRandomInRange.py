88ad52d9eedab99571f626a69d07cbff1cd3cf04
Wu-Clone <wuli77917@gmail.com>
2023-04-20 14:47:03 +0800

    20230420 generateRandomInRange.py

---
+++ 20230420_generateRandomInRange.py
@@ -0,0 +1,7 @@
+# Generate a certain amount of random numbers within a certain range
+import random
+
+min = int(input("Input min:"))
+max = int(input("Input max:"))
+num = int(input("Input num:"))
+print(random.sample(range(min, max), num))
